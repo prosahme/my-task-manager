1756dcf774bb23579ba8c38cb36dc542638fd80c
prosahme <emanhayatabas1920@gmail.com>
2025-08-19 07:22:25 +0300

    All startup and basic UI

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./pages/Home.jsx";
+import Header from "./components/Layout/Header.jsx";
+import Footer from "./components/Layout/Footer.jsx";
+
+export default function App() {
+  return (
+    <div className="app-shell">
+      <div className="bg-orb orb-1" aria-hidden="true" />
+      <div className="bg-orb orb-2" aria-hidden="true" />
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
